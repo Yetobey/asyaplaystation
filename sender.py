21c8fd86072eb6a5f0b09cc4c4a97c2c2f37b4fc
Yetobey <115027841+Yetobey@users.noreply.github.com>
2022-10-04 20:02:48 +0300

    Add files via upload

---
+++ sender.py
@@ -0,0 +1,22 @@
+import time
+import socket
+
+
+def send(ip, port, file):
+    for i in range(3):
+        print(f"Sending payload: attempt {i}...")
+        time.sleep(1)
+        client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM, proto=0)
+        client_socket.settimeout(3000)
+        client_socket.connect((ip, port))
+
+        try:
+            with open(file, "rb") as fp:
+                client_socket.sendfile(fp)
+            print(f"Sending payload: attempt {i} succeeded!")
+            break
+        except Exception as e:
+            print(f"Attempt {i} failed with exception: {str(e)}")
+            pass
+        finally:
+            client_socket.close()
